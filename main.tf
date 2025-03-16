aa7bd66f515b916b734756675353ca1b8c16a4dc
amithg1 <amithg2502@gmail.com>
2025-03-17 01:03:27 +0530

    Added permissions to lambda to configure itself in a VPC.

--- main.tf
+++ main.tf
@@ -105,6 +105,36 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_policy" "lambda_vpc_policy" {
+  name        = "LambdaVPCPolicy"
+  description = "Permissions for Lambda to interact with VPC"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:CreateNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+          "ec2:AssignPrivateIpAddresses",
+          "ec2:UnassignPrivateIpAddresses",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeVpcs"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc_attach" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_vpc_policy.arn
+}
+
 resource "aws_iam_policy" "lambda_kinesis_policy" {
   name        = "LambdaKinesisPolicy"
   description = "Policy for Lambda to read from Kinesis stream"
